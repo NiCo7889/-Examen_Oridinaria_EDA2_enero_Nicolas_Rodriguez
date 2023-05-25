97e22ec08c2a9913407b4feb3880cf2674a68f29
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2023-05-25 10:37:16 +0200

    cambios

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -2,7 +2,7 @@
 El Profesor Oak le encarga desarrollar los algoritmos para organizar los Pokemon cumpliendo con las siguientes demandas:
 
 Deberá generar 800 Pokemon siguiendo el formato del primer ejercicio contemplando los siguientes tipos:
-Agua, Fuego, Tierra, Electrico, Normal, Fantasma y los niveles generados de manera aleatoria;
+- Agua, Fuego, Tierra, Electrico, Normal, Fantasma y los niveles generados de manera aleatoria;
 Deberá cargar los Pokemon generados en dos tablas hash encadenadas, en la primera se deberá agrupar de acuerdo con los tres últimos dígitos del nivel y en la segunda a partir 
 de las iniciales del tipo;
 Determinar si el Pokemon Fantasma de nivel 187 está cargado para poder quitarlo porque es un traidor desertor.
--- Ejercicio8.py
+++ Ejercicio8.py
@@ -24,8 +24,6 @@ Descubre el mensaje cifrado
 """
 
 
-
-# Primero, definimos la clase Nodo
 class Nodo:
     def __init__(self, simbolo, frecuencia, izq=None, der=None):
         self.simbolo = simbolo
@@ -33,80 +31,50 @@ class Nodo:
         self.izq = izq
         self.der = der
 
-# Luego, creamos la función para construir el árbol de Huffman
 def construir_arbol_huffman(simbolos):
     while len(simbolos) > 1:
-        # Ordenamos los símbolos por frecuencia
         simbolos = sorted(simbolos, key=lambda x: x.frecuencia)
-        # Creamos un nuevo nodo con los dos nodos de menor frecuencia
         izq = simbolos.pop(0)
         der = simbolos.pop(0)
         nodo = Nodo(None, izq.frecuencia + der.frecuencia, izq, der)
         simbolos.append(nodo)
-    return simbolos[0]  # El último nodo es la raíz del árbol
+    return simbolos[0]
 
-# Creamos una función para generar los códigos Huffman a partir del árbol
-def generar_codigos(nodo, prefijo='', codigo={}):
+def generar_codigos(nodo, prefijo='', codigo={}, invertir_codigo={}):
     if nodo is None:
         return
     if nodo.simbolo is not None:
         codigo[nodo.simbolo] = prefijo
-    generar_codigos(nodo.izq, prefijo + '0', codigo)
-    generar_codigos(nodo.der, prefijo + '1', codigo)
-    return codigo
+        invertir_codigo[prefijo] = nodo.simbolo
+    generar_codigos(nodo.izq, prefijo + '0', codigo, invertir_codigo)
+    generar_codigos(nodo.der, prefijo + '1', codigo, invertir_codigo)
+    return codigo, invertir_codigo
 
-# Finalmente, creamos las funciones para codificar y decodificar el mensaje
 def codificar(mensaje, codigo):
     return ''.join([codigo[c] for c in mensaje])
 
-def decodificar(mensaje_codificado, nodo):
+def decodificar(mensaje_codificado, invertir_codigo):
     mensaje = ''
-    n = len(mensaje_codificado)
-    i = 0
-    while i < n:
-        nodo_actual = nodo
-        while nodo_actual.simbolo is None:
-            if mensaje_codificado[i] == '0':
-                nodo_actual = nodo_actual.izq
-            else:
-                nodo_actual = nodo_actual.der
-            i += 1
-        mensaje += nodo_actual.simbolo
+    codigo_actual = ''
+    for bit in mensaje_codificado:
+        codigo_actual += bit
+        if codigo_actual in invertir_codigo:
+            mensaje += invertir_codigo[codigo_actual]
+            codigo_actual = ''
     return mensaje
 
-# Ahora podemos usar estas funciones para construir el árbol de Huffman y codificar y decodificar mensajes
-frecuencias = {
-    'T': 0.15,
-    'O': 0.15,
-    'A': 0.12,
-    'E': 0.10,
-    'H': 0.09,
-    'S': 0.07,
-    'P': 0.07,
-    'M': 0.07,
-    'N': 0.06,
-    'C': 0.06,
-    'D': 0.05,
-    'Z': 0.04,
-    'K': 0.03,
-    ',': 0.03,
-    'B': 0.02,  # Añade la frecuencia para 'B'
-    'R': 0.02   # Añade la frecuencia para 'R'
-}
 
-nodos = [Nodo(s, f) for s, f in frecuencias.items()]
-arbol_huffman = construir_arbol_huffman(nodos)
-codigos_huffman = generar_codigos(arbol_huffman)
+frecuencia = {'T': 0.15, 'O': 0.15, 'A': 0.12, 'E': 0.10, 'H': 0.09, 'S': 0.07, 'P': 0.07, 'M': 0.07, 'N': 0.06, 'C': 0.06, 'D': 0.05, 'Z': 0.04, 'K': 0.03, ',': 0.03}
 
 
 if __name__ == "__main__":
 
-    mensaje = "SE,PASA"
-    mensaje_codificado = codificar(mensaje, codigos_huffman)
-    mensaje_decodificado = decodificar(mensaje_codificado, arbol_huffman)
-    mensaje_cifrado = "001101110110100110011010"
+    arbol_huffman = construir_arbol_huffman([Nodo(simbolo, frecuencia) for simbolo, frecuencia in frecuencia.items()])
+    codigos, invertir_codigos = generar_codigos(arbol_huffman)
 
-    print("Mensaje original: ", mensaje)
-    print("Mensaje codificado: ", mensaje_codificado)
-    print("Mensaje decodificado: ", mensaje_decodificado)
-    print("Mensaje cifrado: ", decodificar(mensaje_cifrado, arbol_huffman))
+    texto = 'HAZTE,CON,TODOS,POKEMON'
+    texto_comprimido = codificar(texto, codigos)
+    print('Texto comprimido:', texto_comprimido)
+
+    texto_descomprimido = decodificar(texto_comprimido, invertir_codigos)
+    print('Texto descomprimido:', texto_descomprimido)
